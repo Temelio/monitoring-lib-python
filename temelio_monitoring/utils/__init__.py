8841650db53ae8cde48558b9076de0a97da766be
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2016-06-11 18:22:45 +0200

    Add OperatorUtils class

--- temelio_monitoring/utils/__init__.py
+++ temelio_monitoring/utils/__init__.py
@@ -2,6 +2,8 @@
 Manage utils class import
 """
 
+from . import OperatorUtils
 from . import RequestsUtils
 
+OperatorUtils = OperatorUtils.OperatorUtils
 RequestsUtils = RequestsUtils.RequestsUtils
