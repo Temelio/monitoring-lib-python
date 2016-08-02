17bf6ed7fe675daa3c3fe4c394c3d4937dc71dd7
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2016-08-03 01:24:16 +0200

    Add section parser

--- temelio_monitoring/cli_parser/metric/__init__.py
+++ temelio_monitoring/cli_parser/metric/__init__.py
@@ -3,5 +3,7 @@ Manage argparse modules import
 """
 
 from .metric_name_parser import MetricNameParser
+from .section_name_parser import SectionNameParser
 
 MetricNameParser = MetricNameParser
+SectionNameParser = SectionNameParser
